1ffab26fd6b2f67c87f4913c3874a3137a8dc737
Taylor Want <taylor.want@gmail.com>
2017-08-08 15:43:51 +0000

    added new ruby file and completed the to-do

---
+++ stringstodo.rb
@@ -0,0 +1,7 @@
+puts "Who is the guest??"
+guest_name=gets.chomp
+puts "What is the party called?"
+party_name=gets.chomp
+puts "What is the date of your party?"
+date=gets.chomp
+puts "Dear #{guest_name}, you are invited to #{party_name} on #{date}"
